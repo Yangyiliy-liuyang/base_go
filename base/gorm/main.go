308905c5b3ac79b992f24d291608af333fe226f0
yangyiliy <yangyiliy@foxmail.com>
2023-11-21 12:48:41 +0800

    update transaction、hook

--- base/gorm/main.go
+++ base/gorm/main.go
@@ -29,7 +29,7 @@ func main() {
 		panic("failed to connect database")
 	}
 	db = db.Debug()
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Good{})
 	//users := []User{
 	//	User{Name: "zhangsan", Age: 18, Birthday: time.Now()},
 	//	User{
@@ -66,22 +66,22 @@ func main() {
 	//	println(err)
 	//}
 
-	db.Create(&User{
-		Model:    gorm.Model{},
-		Name:     "",
-		Age:      0,
-		Birthday: time.Time{},
-	})
-	var user User
-	db.Where("age", 12).Find(&user)
-	db.Model(&User{}).Where("age", 0).Update("age", 13)
-	db.Where("id", 0).Delete(&User{})
-	//指定数据库表名
-	err = db.AutoMigrate(&AdminUsers{})
-	if err != nil {
-		return
-	}
-	db.Table("admin_users").Create(&AdminUsers{Admin: true})
+	//db.Create(&User{
+	//	Model:    gorm.Model{},
+	//	Name:     "",
+	//	Age:      0,
+	//	Birthday: time.Time{},
+	//})
+	//var user User
+	//db.Where("age", 12).Find(&user)
+	//db.Model(&User{}).Where("age", 0).Update("age", 13)
+	//db.Where("id", 0).Delete(&User{})
+	////指定数据库表名
+	//err = db.AutoMigrate(&AdminUsers{})
+	//if err != nil {
+	//	return
+	//}
+	//db.Table("admin_users").Create(&AdminUsers{Admin: true})
 
 	//会话 事务
 	err = db.Session(&gorm.Session{}).Transaction(func(*gorm.DB) error {
@@ -100,4 +100,29 @@ func main() {
 	//AfterCreate
 	//AfterSave
 	//// 提交或回滚事务
+	good := Good{
+		Id:   2,
+		Name: "zhangsan",
+	}
+	err = db.Transaction(func(tx *gorm.DB) error {
+		err = tx.Create(&good).Error
+		return nil
+	})
+	if err != nil {
+		println(err)
+	}
+}
+
+type Good struct {
+	Id   int
+	Name string
+}
+
+func (*Good) AfterCreate(tx *gorm.DB) error {
+	print("-----after create ......")
+	return nil
+}
+func (*Good) BeforeCreate(*gorm.DB) error {
+	print("-----before create ......")
+	return nil
 }
